6e0ea7dd28d1ff16d90504a093f82c69713156fd
Oleksandr Fetisov <sasafetisov03@gmail.com>
2025-09-02 15:22:41 +0300

    initial commit

---
+++ src/main/java/com/example/accomodationbookingservice/AccomodationBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.accomodationbookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccomodationBookingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccomodationBookingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/accomodationbookingservice/AccomodationBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accomodationbookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccomodationBookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
